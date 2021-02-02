f3354fe0105643bfe20cbac9df8148493328acc2
Hubnester <jamesdolphin001@gmail.xom>
2021-02-02 21:43:52 +1000

    Race display names

--- zb/newSail/fs_customFunctions.lua
+++ zb/newSail/fs_customFunctions.lua
@@ -1,33 +1,15 @@
+require "/frackinship/scripts/frackinshiputil.lua"
+
 function changeSailImage()
 	widget.clearListItems("root.miscList")
 	GUI.frackinship.sailSelect = true
 	local sailList = root.assetJson("/ai/ai.config").species or {}
-	local races = {}
-	for race, _ in pairs (sailList) do
-		table.insert(races, race)
-		local succeded, raceData = pcall(root.assetJson, "/species/" .. race .. ".species")
-		if succeded and raceData and raceData.charCreationTooltip then
-			sailList[race].name = raceData.charCreationTooltip.title
-		end
-	end
-	table.sort(races, fsCompareByName)
+	local races = frackinship.getRaceList()
+	local raceDisplayNames = frackinship.getRaceDisplayNames(races)
 	for _, race in pairs (races) do
 		local listItem = "root.miscList."..widget.addListItem("root.miscList")
-		widget.setText(listItem..".name", sailList[race].name)
+		widget.setText(listItem..".name", raceDisplayNames[race] or race)
 		widget.setImage(listItem..".icon", "/ai/" .. sailList[race].aiFrames .. ":idle?scalenearest=0.2")
 		widget.setData(listItem, {race = race})
 	end
-end
-
--- Copied from terminal code
-function fsCompareByName(raceA, raceB)
-	local a = fsComparableName(raceA)
-	local b = fsComparableName(raceB)
-	return a < b
-end
-
-function fsComparableName(name)
-	return name:gsub('%^#?%w+;', '') -- removes the color encoding from names, e.g. ^blue;Madness^reset; -> Madness
-		:gsub('ū', 'u')
-		:upper()
 end
